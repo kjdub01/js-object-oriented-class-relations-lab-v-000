f9ce9f19f16f959d6a674626812f9cf8e9b6bf96
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-05-21 01:15:30 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,37 @@
+let store = { drivers: [], passengers: [], trips: []};
+
+let driverId = 0;
+
+class Driver {
+  constructor(name) {
+    this.id = ++driverId;
+    this.name = this;
+    
+    store.drivers.push(this);
+  }
+  
+  
+}
+
+let passengerId = 0;
+
+class Passenger {
+  constructor(name) {
+    this.id = ++passengerId;
+    this.name = name;
+    
+    store.passengers.push(this);
+  }
+  
+  
+}
+
+let tripId = 0;
+
+class Trip {
+  constructor(driverId, passengerId) {
+    this.id = ++tripId;
+    this.driverId = driverId;
+    this.passengerId = passengerId;
+  }
+}
